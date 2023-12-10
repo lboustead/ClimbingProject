91aff8691db5720c719dbb116fee73b8485b60cf
Lucas Boustead <lucasboustead@gmail.com>
2023-12-09 23:13:12 -0600

    AHHHHHH

--- ClimbingProject/Home.aspx.cs
+++ ClimbingProject/Home.aspx.cs
@@ -26,24 +26,15 @@ namespace ClimbingProject
 
             //get full name query
             string getFullNameQuery = "SELECT CONCAT(FirstName,' ',LastName) FROM Climber WHERE Climber.ClimberID = '"+climberID+"'";
-
-            string highestGradeSentQuery = "SELECT GradeDescription FROM Grades WHERE Grade_ID in (SELECT MAX(Grade_ID) FROM Grades, NetUser, Route, Sends WHERE NetUser.ClimberID = '"+climberID+"' AND Sends.RouteID = Route.RouteID AND Route.GradeID = Grades.Grade_ID)";
-            
-            string highestGradeAttemptQuery = "";
-            string averageGradeSentQuery = "";
+            string highestGradeSentQuery = "SELECT GradeDescription FROM Grades WHERE Grade_ID in (SELECT MAX(Grade_ID) FROM Grades, NetUser, Route, Sends WHERE NetUser.ClimberID = '"+climberID+"' AND NetUser.ClimberID = Sends.ClimberID AND Sends.RouteID = Route.RouteID AND Route.GradeID = Grades.Grade_ID)";
+            string averageGradeSentQuery = "SELECT GradeDescription FROM Grades WHERE Grade_ID in (SELECT ROUND(AVG(Grade_ID), 0) FROM Grades, NetUser, Route, Sends WHERE NetUser.ClimberID = '"+climberID+"' AND NetUser.ClimberID = Sends.ClimberID AND Sends.RouteID = Route.RouteID AND Route.GradeID = Grades.Grade_ID)\r\n";
             string mostClimbedLocationQuery = "";
-
-            string HGS = "";
-            string HGA = "";
-            string AGS = "";
-            string MCL = "";
 
             using (SqlConnection conn = new SqlConnection(connString))
             {
                 conn.Open();
                 SqlCommand getFullName = new SqlCommand(getFullNameQuery, conn);
                 SqlCommand getHighestGradeSent = new SqlCommand(highestGradeSentQuery, conn);
-                SqlCommand getHighestGradeAttempt = new SqlCommand(highestGradeAttemptQuery, conn);
                 SqlCommand getAverageGradeSent = new SqlCommand(averageGradeSentQuery, conn);
                 SqlCommand getMostClimbedLocation = new SqlCommand(mostClimbedLocationQuery, conn);
 
@@ -52,7 +43,10 @@ namespace ClimbingProject
 
                 //setting HighestGradeSent
                 HGSlbl.Text = getHighestGradeSent.ExecuteScalar().ToString();
-                
+
+                //setting AverageGradeSent
+                AGSlbl.Text = getAverageGradeSent.ExecuteScalar().ToString();
+
                 conn.Close();
             }
         }
